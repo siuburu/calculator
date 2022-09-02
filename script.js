d23f96b9f535f0a204701f92d0546d8a68e6c038
Victor <urubuis1@gmail.com>
2022-09-01 20:37:24 -0400

    Added function to update numbers to display

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Operator Functions
 function add(a, b) {
 	return a + b;
 }
@@ -17,3 +18,30 @@ function divide(a, b) {
 function operate(operator, a, b) {
 	return operator(a, b);
 }
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const display = document.querySelector(".display");
+const clearButton = document.querySelector(".clear");
+
+function updateDisplay(number) {
+	display.textContent = display.textContent + number;
+}
+// Clear Display function with 0
+function clearDisplay() {
+	display.textContent = "0";
+}
+// Function to update display with numbers
+numberButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		//
+		if (display.textContent.includes(".") && button.textContent === ".") {
+			return;
+		} else if (display.textContent === "0") {
+			display.textContent = "";
+		}
+		updateDisplay(button.textContent);
+	});
+});
+
+clearButton.onclick = () => clearDisplay();
